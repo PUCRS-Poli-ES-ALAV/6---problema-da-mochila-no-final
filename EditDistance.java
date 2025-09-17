d6fc08f11cbc7509624baca4e0a37d8c3cd57c2a
FernandoJungLau <fejunglau@gmail.com>
2025-09-17 23:24:18 +0000

    dist recursivo

--- EditDistance.java
+++ EditDistance.java
@@ -1,16 +1,26 @@
 public class EditDistance {
 
 
-    public static int getDistance(String a, String b, int i, int j) {
-        int dist = 0;
-
-
-        return dist;
+    public static int getDistance(String S, String T, int i, int j) {
+        if (i == -1 && j == -1) {
+            return 0;
+        } else if (i == -1) {
+            return T.length();
+        } else if (j == -1) {
+            return S.length();
+        }
+        if (S.charAt(i) == T.charAt(j)) { 
+            return getDistance(S, T, i-1, j-1);
+        } else {
+            return Math.min(getDistance(S, T, i-1, j-1) + 1, Math.min(getDistance(S, T, i, j-1) + 1, getDistance(S, T, i-1, j) + 1));
+        }
 
     }
 
     public static void main(String[] args) {
-        System.out.println(getDistance("Casablanca", "Portentoso"));  
+        String S = "bola";
+        String T = "bala";
+        System.out.println(EditDistance.getDistance(S, T, S.length()-1, T.length()-1));  
     }
 }
 /*
@@ -19,7 +29,7 @@ Quando ficamos sem caracteres para comparer em S ou em T. Se em ambas, o
 resultado é 0. Se uma das duas, retorna o restante dos caracteres da que não
 está vazia;
 Casos Recursivos
-Se S[i] == T[i], chame recursivamente ED(S, T, i-1, j-1) (foi match, não
+Se S[i] == T[j], chame recursivamente ED(S, T, i-1, j-1) (foi match, não
 precisa fazer nada nesta posição, o custo é zero.
 Se não, três chamadas recursivas são necessárias:
 • Substituição: ED(S, T, i-1, j-1) + 1
